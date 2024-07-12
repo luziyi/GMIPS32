017d4c440b826b49a2e477b2c176e724351d1751
TommyGong <luziyi.lucy@gmail.com>
2024-07-12 10:07:38 +0800

    feat(stall): add stall control

---
+++ thinpad_top.srcs/sources_1/GMIPS/stall.v
@@ -0,0 +1,18 @@
+/*
+ * File:         e:\NSCSCC\GMIPS\thinpad_top.srcs\sources_1\GMIPS\stall.v
+ * Project:      e:\NSCSCC\GMIPS\thinpad_top.srcs\sources_1\GMIPS
+ * Created Date: 2024-07-12 09:56:33
+ * Author:       Tommy Gong
+ * description:  
+ * ----------------------------------------------------
+ * Last Modified: 2024-07-12 09:56:50
+ */
+
+module stall_ctrl (
+    input wire rst,
+    input wire stallreq_from_id,
+    input wire stallreq_from_baseram,
+    output wire stall
+);
+  assign stall = stallreq_from_id | stallreq_from_baseram;
+endmodule 
